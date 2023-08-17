f51f6f2c2520552b22714aaab240f732b08197c5
RuanRDM <ruan_delatorre@hotmail.com>
2023-08-17 09:31:07 -0300

    att antes intervalo 3

--- lib/screens/form.dart
+++ lib/screens/form.dart
@@ -2,25 +2,33 @@ import 'package:flutter/material.dart';
 import '../components/editor.dart';
 import '../models/tarefa.dart';
 
-class FormTarefa extends StatelessWidget {
+class FormTarefa extends StatefulWidget {
   final TextEditingController controladorTarefa = TextEditingController();
   final TextEditingController controladorObs = TextEditingController();
 
+  @override
+  State<StatefulWidget> createState() {
+    return FormTarefaState();
+  }
+}
+
+class FormTarefaState extends State<FormTarefa>{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           title: Text("Form de Tarefas"),
         ),
-        body: Padding(
+        body: SingleChildScrollView(
+         child: Padding(
             padding: const EdgeInsets.all(16.0),
             child: Column(
               children: <Widget>[
-                Editor(controladorTarefa,
+                Editor(widget.controladorTarefa,
                     "Tarefa",
                     "Informe a tarefa",
                     Icons.assignment),
-                Editor(controladorObs,
+                Editor(widget.controladorObs,
                     "Obs",
                     "Informe a descrição",
                     Icons.assignment),
@@ -29,11 +37,12 @@ class FormTarefa extends StatelessWidget {
                 }, child: Text("Salvar"))
               ],
             )
+          )
         )
     );
   }
   void criarTarefa(BuildContext context){
-    final tarefa = Tarefa(controladorTarefa.text, controladorObs.text);
+    final tarefa = Tarefa(widget.controladorTarefa.text, widget.controladorObs.text);
     Navigator.pop(context, tarefa);
     SnackBar snackBar = SnackBar(content: Text("Tarefa criada com sucesso!"));
     ScaffoldMessenger.of(context).showSnackBar(snackBar);
